cc5b738945d8e946b002c106df16c9586d05962f
tannahic <38050790+tannahic@users.noreply.github.com>
2021-05-20 17:31:59 -0400

    Update db_credentials.py

--- database/db_credentials.py
+++ database/db_credentials.py
@@ -2,5 +2,5 @@
 class dbCred():
     host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
     user = 'cs340_tannahic'       # don't forget the CS_340 prefixs
-    passwd = '3056'               # should only be 4 digits if default
+    passwd = ''               # should only be 4 digits if default
     db = 'cs340_tannahic'                
